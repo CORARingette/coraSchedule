0a16568c51fe0f2e86df7b68f990d611fff66761
Andrew McGregor <mcgregorjohnandrew@gmail.com>
2022-09-23 11:18:49 -0400

    Updates for end of tryouts

--- RingetteSchedule/src/utils/Config.java
+++ RingetteSchedule/src/utils/Config.java
@@ -29,7 +29,7 @@ public class Config {
 			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = builderFactory.newDocumentBuilder();
 
-			Document document = builder.parse(new FileInputStream(Paths.get("properties", "TeamConfig.2022-2023-Tryouts.xml").toString()));
+			Document document = builder.parse(new FileInputStream(Paths.get("properties", "TeamConfig.2022-2023.xml").toString()));
 
 			XPath xPath = XPathFactory.newInstance().newXPath();
 
