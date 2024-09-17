b6a0e60b1580dccdd3745a831729534cd518b90a
Andrew McGregor <mcgregorjohnandrew@gmail.com>
2024-09-16 23:01:48 -0400

    Updated to 2024-2025 team list

--- RingetteSchedule/src/utils/Config.java
+++ RingetteSchedule/src/utils/Config.java
@@ -29,7 +29,7 @@ public class Config {
 			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = builderFactory.newDocumentBuilder();
 
-			Document document = builder.parse(new FileInputStream(Paths.get("properties", "TeamConfig.2023-2024.xml").toString()));
+			Document document = builder.parse(new FileInputStream(Paths.get("properties", "TeamConfig.2024-2025.xml").toString()));
 			//Document document = builder.parse(new FileInputStream(Paths.get("properties", "TeamConfigTest.xml").toString()));
 
 			XPath xPath = XPathFactory.newInstance().newXPath();
